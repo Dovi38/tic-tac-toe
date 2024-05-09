0ca9789d6c922d87eebeb50848cddb0238f800f5
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2024-05-09 21:09:51 +0100

    Can count and print signs in array

--- script.js
+++ script.js
@@ -2,22 +2,25 @@ const gridElement = document.querySelector(".grid-container");
 const grid = document.querySelectorAll(".square");
 //board size for array
 let size = 9;
-let pattern = "";
+
 let countX = 0;
 let countO = 0;
-let sign_X = "x";
-let sign_O = "0";
+let sign_X = "X";
+let sign_O = "O";
+let previousSign = "";
 //1.board array render  with 9 empty strings.
-let board = Array(size).fill("");
+const board = Array(size).fill("");
 console.log(board.length);
-console.log((board[1] = "x"));
+
+//console.log((board[1] = "X"));
 //2.checks if all strings in a array is empty
 const checkEmptyStrings = (board, countX = 0, countO = 0) => {
+  console.log(board);
   board.forEach((str) => {
     if (str !== "") {
-      if (str === "x") {
+      if (str === "X") {
         countX++;
-      } else if (str === "0") {
+      } else if (str === "O") {
         countO++;
       }
     }
@@ -26,15 +29,66 @@ const checkEmptyStrings = (board, countX = 0, countO = 0) => {
   console.log(countO);
   return board;
 };
-console.log(checkEmptyStrings(board, countX, countO));
+checkEmptyStrings(board, countX, countO);
+//1.Start and play game
+const checksPreviousSign = (previousSign, board) => {
+  let copyBoard = [...board];
+  console.log(copyBoard);
+  copyBoard.forEach((el, index) => {
+    if (el === "" && previousSign === "") {
+      copyBoard[index] = sign_X;
+      previousSign = sign_X;
+    } else if (el === "" && previousSign === "X") {
+      copyBoard[index] = sign_O;
+      previousSign = sign_O;
+    } else {
+      copyBoard[index] = sign_X;
+      previousSign = sign_X;
+    }
+  });
+  console.log(copyBoard);
+  return checkEmptyStrings(copyBoard);
+};
+checksPreviousSign(previousSign, board);
+
 //Check if one of counter reaches 3
 const counterCheck = (countX, countO) => {
   if (countX === 3) {
+    //iterate through array and compare indexes of array with array of arrays(wincombination)
     //function which calculates winCombination
   } else if (countO === 3) {
     //function which calculates winCombination
   }
 };
+
+// const checksPreviousSign = (previousSign, board) => {
+//   const newBoard = board.slice(); // Create a copy
+
+//   newBoard.forEach((el, index) => {
+//     if (el === "" && previousSign !== "") {
+//       newBoard[index] = previousSign === sign_X ? "o" : "x";
+//       // Assign based on previous sign
+//       console.log(newBoard[index]);
+//     }
+//   });
+
+//   return newBoard;
+// };
+// const checkPreviousSign = (previousSign, board) => {
+//   // Create a copy of the board to avoid modifying the original
+//   const copyBoard = board.slice(); // Or use spread syntax: [...board]
+
+//   // Loop through each cell in the board
+//   copyBoard.forEach((el, index) => {
+//     if (el === "" && previousSign === "") {
+//       copyBoard[index] = "X"; // Use uppercase X for clarity
+//     } else if (el === "" && previousSign === "X") {
+//       copyBoard[index] = "O";
+//     }
+//   });
+
+//   return copyBoard;
+// };
 const winCombinations = [
   [0, 1, 2],
   [3, 4, 5],
@@ -48,30 +102,13 @@ const winCombinations = [
 
 console.log(grid);
 
-console.log(board);
-console.log((board[3] = "x"));
-console.log(board);
-let move = board.indexOf("x");
-console.log(move);
-
 const squareClicked = () => {
   for (let i = 0; i <= board.length; i++) {
     let click = board[i] !== "";
-    console.log(click);
   }
 };
 squareClicked();
-
-console.log((board[0] = "0"));
-console.log(board);
-let secBoard = board.filter((el) => el === "x");
-console.log(secBoard.length);
-console.log(board);
-
-secBoard = board.filter((el) => el === "x");
-console.log(secBoard);
-console.log(secBoard.length);
-
+//let pattern = "";
 // for (let i = 1; i <= size; i++) {
 //   for (let j = 1; j <= 3; j++) {
 //     //console.log((pattern += grid));
